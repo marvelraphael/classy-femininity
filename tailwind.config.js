88ae0f56e3e87adfe44ba151527af829c235aae5
marvelraphael <136002480+marvelraphael@users.noreply.github.com>
2025-05-25 22:17:39 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#b20235", // deep pink
+      },
+    },
+  },
+  plugins: [],
+};
